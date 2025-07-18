fc6c8a4ae117deafb21daa7a29ef83405c808613
virtual_pancakes <virtual_pancakes@outlook.com>
2025-07-18 14:26:29 +0200

    implenting update function. Commit before starting to download and replace files

--- MetaHumanExtraTools/plugin/MetaHumanExtraTools.py
+++ MetaHumanExtraTools/plugin/MetaHumanExtraTools.py
@@ -6,34 +6,39 @@ import subprocess
 import json
 import os
 import sys
+import urllib.request
 
 def initializePlugin(plugin):
     #om2.MFnPlugin(plugin)
     
+    """
     # Install requests module to be able to check for updates
     if "requests" not in [p.project_name for p in pkg_resources.working_set]:
         subprocess.run(["mayapy", "-m", "pip", "install", "requests"]) 
+    """
     
     # Check if new version available
-    met_path = False
-    for path in sys.path:
-        if "MetaHumanExtraTools" in path: 
-            met_path = path
-            break
-    import requests
-    local_json_path = os.path.join(os.path.abspath(met_path), "version.json")
-    live_json_link = "https://raw.githubusercontent.com/virtual-pancakes/met/refs/heads/main/MetaHumanExtraTools/version.json"
-    local_dict = json.load(open(local_json_path, "r"))
-    live_dict = requests.get(live_json_link).json()
-    local_version = local_dict["current_version"]
-    live_version = live_dict["current_version"]
-    if local_version != live_version: 
-        print("there is a new version available")
-        local_dict["newest_version"] = live_version = live_dict["current_version"]
-        local_dict["newest_changes"] = live_version = live_dict["current_changes"]
-        json.dump(local_dict, open(local_json_path, "w"), indent=4)
-    else:
-        print("version up to date")
+    try:
+        met_path = False
+        for path in sys.path:
+            if "MetaHumanExtraTools" in path: 
+                met_path = path
+                break
+        local_json_path = os.path.join(os.path.abspath(met_path), "version.json")
+        live_json_link = "https://raw.githubusercontent.com/virtual-pancakes/met/refs/heads/main/MetaHumanExtraTools/version.json"
+        local_dict = json.load(open(local_json_path, "r"))
+        live_dict = json.loads(urllib.request.urlopen(live_json_link).read().decode('utf-8'))
+        local_version = local_dict["current_version"]
+        live_version = live_dict["current_version"]
+        if local_version != live_version: 
+            print("MetaHumanExtraTools: there is a new version available")
+            local_dict["newest_version"] = live_version = live_dict["current_version"]
+            local_dict["newest_version_changes"] = live_version = live_dict["current_version_changes"]
+            json.dump(local_dict, open(local_json_path, "w"), indent=4)
+        else:
+            print("MetaHumanExtraTools: version up to date")
+    except:
+        print("MetaHumanExtraTools: unable to check for updates, please check manually")
     
     # Load MetaHumanForMaya
     try:
